1adcdb1de4892ec65542b693e3ef52b6c45b0d35
Alexey <alexeyhenke@yandex.ru>
2026-06-10 20:36:52 +0300

    [tasks] add extra field 'user_id'

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,12 @@
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User # Импортируем модель пользователя
 
 class Task(models.Model):
+    # Новое поле: связь с пользователем.
+    # models.CASCADE означает: если удалить пользователя, удалятся и все его задачи.
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
     # Заголовок задачи, максимум 200 символов
     title = models.CharField(max_length=200)
     # Статус: выполнена задача или нет (по умолчанию — нет)
